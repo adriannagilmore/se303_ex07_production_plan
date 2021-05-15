906f83780fdbbcf1e2c8a8210fbaf73557627dbf
Adrianna Gilmore <a_inman33@yahoo.com>
2021-05-15 10:51:47 -0700

    added no_producers

--- test/province_test.rb
+++ test/province_test.rb
@@ -5,7 +5,7 @@ require_relative '../lib/province'
 require_relative '../lib/data'
 
 class ProvinceTest < Minitest::Test
-  #let(:asia) {Province.new(sample_province_data)}
+
   def test_province_shortfall
     asia = Province.new(sample_province_data)
     assert_equal(5, asia.shortfall)
@@ -22,5 +22,14 @@ class ProvinceTest < Minitest::Test
     assert_equal(-6, asia.shortfall)
     assert_equal(292, asia.profit)
   end
+
+  def no_producers
+    data = { name: "No Producers", producers: [], demand: 30, price: 20 }
+    noProducers = Province.new(data)
+    assert_equal(30, noProducers.shortfall)
+    assert_equal(0, noProducers.profit)
+  end
+
+
 
 end
